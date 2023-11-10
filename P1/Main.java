5139742563477b062f88697609fd05ea6e82f8d8
fidha27 <140831005+fidha27@users.noreply.github.com>
2023-11-10 19:04:05 +0530

    Create Main.java

---
+++ P1/Main.java
@@ -0,0 +1,14 @@
+package main;
+import packagegone.A;
+import packagetwo.B;
+public class Main
+{
+  public static void main(String[] args) 
+  {
+    A obj1=new A();
+    obj1.methodclassone();
+    B obj2=new B();
+    obj2.methodclasstwo();
+
+  }
+}  
